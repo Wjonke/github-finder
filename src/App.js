0c199fb32e0496c400e220f8076db55c9dff7ea9
Wes Jonke <wesjonke@gmail.com>
2019-06-06 22:47:31 -0400

    testing to make sure sync with github is working

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ class App extends React.Component {
    
     return (
       <div className="App">
-        <Navbar />{/*this would normally have props defined here, however, the prop defaults are defined in Navbar. 
+        <Navbar />{/*this would normally have props defined here, however, the prop defaults are defined in Navbar.js 
                   If I wanted to change the props I could override defaults by defining them inside <Navbar/>  */}
 
         <div className="container">{/*this is a prestyled container from css for everything except the nav bar */}
           <Users />
-        </div>
+        </div>{/*this is a prestyled container from css for everything except the nav bar */}
         
       </div>
     );
